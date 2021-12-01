d33523db6beba08dac38f8613d8cd09ca06af905
Ivan Galban <ivan.galban.smith@gmail.com>
2021-12-01 15:34:46 -0600

    Solve day 1

---
+++ src/advent_of_code/2021/day1.clj
@@ -0,0 +1,19 @@
+(ns advent-of-code.2021.day1
+  (:require [clojure.string :as str]))
+
+(def input (->> (slurp "./resources/2021/input1.txt")
+                str/split-lines
+                (map parse-long)))
+
+(def ans1 (->> (map - (rest input) input)
+               (filter pos?)
+               count))
+ans1 ; => 1374
+
+;; part2
+
+(def ans2 (as-> (map + (drop 2 input) (drop 1 input) input) $
+                (map - (rest $) $)
+                (filter pos? $)
+                (count $)))
+ans2 ; => 1418
