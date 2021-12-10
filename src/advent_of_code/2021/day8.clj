e33dff9749bc24e3989109833dc0b4ef6add1635
Ivan Galban <ivan.galban.smith@gmail.com>
2021-12-09 18:37:46 -0600

    Solve day8

---
+++ src/advent_of_code/2021/day8.clj
@@ -0,0 +1,119 @@
+(ns advent-of-code.2021.day8
+  (:require [clojure.string :as str]))
+
+(def sample (slurp  "./resources/2021/sample8.txt"))
+
+(def input (slurp "./resources/2021/day8.txt"))
+
+(defn sort-str [s]
+  (apply str (sort s)))
+
+(defn parse [input]
+  (->> input
+       str/split-lines
+       (map #(str/split % #" \| "))
+       (map (fn [[s o]] [(str/split s #" ") (str/split o #" ")]))
+       (map (fn [[s o]] [(mapv sort-str s) (mapv sort-str o)]))
+       (map (fn [[s o]] [(group-by count s) o]))
+       (map (fn [[s o]] [(filter (fn [[_ v]] (= 1 (count v))) s) o]))
+       (map (fn [[s o]] [(mapcat identity (vals s)) o]))
+       (map (fn [[s o]] [(set s) o]))))
+
+(parse sample)
+
+(defn part1
+  ([] (part1 sample))
+  ([input]
+   (->> (parse input)
+        (map (fn [[s o]] (reduce + (map #(if (s %) 1 0) o))))
+        (reduce +))))
+
+;;  0000    0000    ....    2222   3333    ....     5555    6666    7777    8888    9999
+;; 1    2  0    0  .    1  .    2  .    3  4    4  5    .  6    .  .    7  8    8  9    9
+;; 1    2  0    0  .    1  .    2  .    3  4    4  5    .  6    .  .    7  8    8  9    9
+;;  3333   ....    ....    2222    3333     4444    5555    6666    ....    8888    9999
+;; 4    5  0    0  .    1  2    .  .    3  .    4  .    5  6    6  .    7  8    8  .    9
+;; 4    5  0    0  .    1  2    .  .    3  .    4  .    5  6    6  .    7  8    8  .    9
+;;  6666    0000    ....    2222   3333    ....    5555    6666    ....     8888    9999
+
+(def numbers
+  {[0 1 2 4 5 6]   0
+   [2 5]           1
+   [0 2 3 4 6]     2
+   [0 2 3 5 6]     3
+   [1 2 3 5]       4
+   [0 1 3 5 6]     5
+   [0 1 3 4 5 6]   6
+   [0 2 5]         7
+   [0 1 2 3 4 5 6] 8
+   [0 1 2 3 5 6]   9})
+
+(def permutations
+  (for [a (range 0 7)
+        b (range 0 7)
+        :when (not (#{a} b))
+        c (range 0 7)
+        :when (not (#{a b} c))
+        d (range 0 7)
+        :when (not (#{a b c} d))
+        e (range 0 7)
+        :when (not (#{a b c d} e))
+        f (range 0 7)
+        :when (not (#{a b c d e} f))
+        g (range 0 7)
+        :when (not (#{a b c d e f} g))
+        :let [permutation [a b c d e f g]]]
+    permutation))
+
+(defn parse2 [input]
+  (->> input
+       str/split-lines
+       (map #(str/split % #" \| "))
+       (map (fn [[s o]] [(str/split s #" ") (str/split o #" ")]))))
+
+(defn decode [segment code]
+  (->> segment
+       (map #(- (int %) (int \a)))
+       (map #(nth code %))
+       sort))
+
+(defn solution? [code signal]
+  (let [decodes (mapv #(decode % code) signal)]
+    (when (every? numbers decodes)
+      code)))
+
+(defn solve [[signal output]]
+  (let [code (->> permutations
+                  (keep #(solution? % signal))
+                  first)]
+    (map #(numbers (decode % code)) output)))
+
+(def sample2
+  "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce")
+
+(defn part2
+  ([] (part2 sample2))
+  ([input]
+   (->> (parse2 input)
+        (map solve)
+        (map #(apply str %))
+        (map parse-long)
+        (reduce +))))
+
+(defn main []
+  (println "sample 1: " (part1))
+  (println "input 1: " (part1 input))
+  (println "sample 2: " (part2))
+  (println "input 2: " (part2 input)))
+
+(comment
+  (main))
